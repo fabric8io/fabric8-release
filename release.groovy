f37934954a9545995778dcc083679b3456273bcf
rawlingsj <rawlingsj80@gmail.com>
2017-03-10 23:30:56 +0000

    first commit

---
+++ release.groovy
@@ -0,0 +1,36 @@
+#!/usr/bin/groovy
+def stage(){
+  return stageProject{
+    project = 'fabric8io/fabric8-release'
+    useGitTagForNextVersion = true
+  }
+}
+
+def approveRelease(project){
+  def releaseVersion = project[1]
+  approve{
+    room = null
+    version = releaseVersion
+    console = null
+    environment = 'fabric8'
+  }
+}
+
+def release(project){
+  releaseProject{
+    stagedProject = project
+    useGitTagForNextVersion = true
+    helmPush = false
+    groupId = 'io.fabric8.release.packages'
+    githubOrganisation = 'fabric8io'
+    artifactIdToWatchInCentral = 'packages'
+    artifactExtensionToWatchInCentral = 'pom'
+    promoteToDockerRegistry = 'docker.io'
+    dockerOrganisation = 'fabric8'
+    imagesToPromoteToDockerHub = []
+    extraImagesToTag = null
+  }
+}
+
+
+return this;
